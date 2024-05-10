8a40c2b346cd613b689fb058fab5a36877d1cc70
CorpseCoder <68142751+CorpseCoder@users.noreply.github.com>
2024-05-10 11:49:06 +0530

    Remove unnecessary delay

--- main-program.py
+++ main-program.py
@@ -1,16 +1,12 @@
 import Linux
 import Windows
 import platform
-from time import sleep
 
 def findOS():
    print("Checking OS, please wait.")
    info=platform.system()
    if info == "Linux":
       print("You use Linux!")
-      sleep(2)
-      print("Launching app, please wait!")
-      sleep(2)
       Linux.backup()
    elif info == "Darwin":
       print("You use macOS!")
@@ -19,7 +15,6 @@ def findOS():
    elif info == "Windows":
       print("You use Windows!")
       Windows.backup()
-      exit()
    else:
       print("I think you use an OS unknown to humanity. Is it FreeBSD? or TempleOS?")
       exit()
