ce318c2f04b52ce23a7cbf131aef1fc994bb9a38
CorpseCoder <subin2981padmanabhan@gmail.com>
2024-05-21 22:55:31 +0530

    Fixed Errors... maybe

--- Linux.py
+++ Linux.py
@@ -7,33 +7,42 @@ compPath = os.path.dirname(__file__)
 ADBPath = f"{compPath}/platform-tools/"
 
 def backup(compPath,ADBPath):
-	try:
-		file=open("backup.txt","r")
-	except:
-		file=open("backup.txt","w")
-		print("Backup txt file is created, please enter the path of files you want to save from phone")
-		file.close()
-	str=file.readlines()
-	file.close()
-	for i in str:
-		i=i.rsplit("\n")
-		phonePath=i[0]
-		try:
-			os.makedirs(compPath+"/Saved_Files")
-		except FileExistsError:
-			pass
-		try:
-			command=f"cd {ADBPath} && ./adb pull {phonePath} {compPath}/Saved_Files/"
+    try:
+        file=open("backup.txt","r")
+    except:
+        file=open("backup.txt","w")
+        print("Backup txt file is created, please enter the path of files you want to save from phone")
+        file.close()
+    str=file.readlines()
+    file.close()
+    for i in str:
+        i=i.rsplit("\n")
+        phonePath=i[0]
+        try:
+            os.makedirs(compPath+"/Saved_Files")
+        except FileExistsError:
+            pass
+        try:
+            if " " in compPath:
+                compPath = compPath.replace(" ","\ ")
+            command=f"cd {ADBPath} && ./adb pull {phonePath} {compPath}/Saved_Files/"
             subprocess.check_output(command, shell=True, text=True, stderr=subprocess.STDOUT)
-			print("Backed up", phonePath)
-		except subprocess.CalledProcessError as e:
-			print(e.output)
-			if "Permission denied" in e.output:
-				print("Enter sudo password to set ADB to be executable")
-				os.system(f"cd {ADBPath} && sudo chmod +x *")
+            print("Backed up", phonePath)
+        except subprocess.CalledProcessError as e:
+            if "Permission denied" in e.output:
+                print("Enter sudo password to set ADB to be executable")
+                os.system(f"cd {ADBPath} && sudo chmod +x *")
+                os.system(command)
+                print("Backed up", phonePath)
+            if "no devices/emulators found" in e.output:
+                print("Plug your device in")
+                os.system("adb kill-server")
+                exit()
 
-		except:
-			command=f"cd {ADBPath} && ./adb pull {phonePath} {compPath}/Saved_Files/ >>/dev/null"
-			os.system(command)
-			print("Backed up", phonePath)
-		sleep(2)
+        except:
+            command=f"cd {ADBPath} && ./adb pull {phonePath} {compPath}/Saved_Files/ >>/dev/null"
+            os.system(command)
+            print("Backed up", phonePath)
+        sleep(2)
+    os.system("adb kill-server")
+    exit()
--- Windows.py
+++ Windows.py
@@ -5,22 +5,32 @@ compPath = os.path.dirname(__file__)
 ADBPath = compPath + "\\" + "platform-tools\\"
 
 def backup(compPath,ADBPath):
-	try:
-		file=open("backup.txt","r")
-	except:
-		file=open("backup.txt","w")
-		print("Backup txt file is created, please enter the path of files you want to save from phone")
-	str=file.readlines()
-	file.close()
-	for i in str:
-		i=i.rsplit("\n")
-		phonePath=i[0]
-		try:
-			os.makedirs(f"{compPath}\\Saved_Files")
-		except FileExistsError:
-			pass
+    try:
+        file=open("backup.txt","r")
+    except:
+        file=open("backup.txt","w")
+        print("Backup txt file is created, please enter the path of files you want to save from phone")
+    str=file.readlines()
+    file.close()
+    for i in str:
+        i=i.rsplit("\n")
+        phonePath=i[0]
+        try:
+            if " " in compPath:
+                compPath = compPath.replace(" ","\ ")
+            os.makedirs(f"{compPath}\\Saved_Files")
+        except FileExistsError:
+            pass
 
-		command=f"cd {ADBPath} && adb pull {phonePath} {compPath}\\Saved_Files\\"
-		os.system(command)
-		print("Backed up",phonePath)
-		sleep(2)
+        try:
+            command=f"cd {ADBPath} && adb pull {phonePath} {compPath}\\Saved_Files\\"
+            subprocess.check_output(command, shell=True, text=True)
+            print("Backed up",phonePath)
+        except subprocess.CalledProcessError as e:
+            if "no devices/emulators found" in e.output:
+                print("Plug your phone in")
+                os.system("adb kill-server")
+                exit()
+        sleep(2)
+    os.system("adb kill-server")
+    exit()
